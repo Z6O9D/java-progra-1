e707b36f8221d8685dd5b5395254db45e114bf1a
Z6O9D <95990845+Z6O9D@users.noreply.github.com>
2023-03-17 09:34:00 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Televisor tv1 = new Televisor();
+    tv1.subirVolumen();
+
+    System.out.println("El volumen del televisor es: " + tv1.getVolumen());
+
+  }
+
+}
