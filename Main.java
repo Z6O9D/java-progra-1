627096b74b1538dcebc5eb13d7dcc5ff02da163e
Z6O9D <95990845+Z6O9D@users.noreply.github.com>
2023-03-23 19:24:29 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,19 @@
 import java.util.*;
 
 public class Main {
-
-  public static void main(String[] args) {
-    Televisor tv1 = new Televisor();
-    tv1.subirVolumen();
-
-    System.out.println("El volumen del televisor es: " + tv1.getVolumen());
-
+    public static void main(String[] args) {
+      Televisor tv1 = new Televisor();
+      Televisor tv2 = new Televisor();
+      
+      tv1.prender();
+     
+      tv1.bajarVolumen();
+      tv1.bajarVolumen();
+      
+      for(int i=0; i<101; i++)
+         tv1.subirVolumen();
+         
+      System.out.println("El volumen del tv es: " + tv1.getVolumen());
+      
   }
-
 }
