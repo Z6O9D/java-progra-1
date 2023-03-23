e29c2891f74122be22f93918709ded13ca391b7b
Z6O9D <95990845+Z6O9D@users.noreply.github.com>
2023-03-23 19:28:23 -0300

    Update Televisor.java

--- Televisor.java
+++ Televisor.java
@@ -1,49 +1,58 @@
-class Televisor {
+public class Televisor {
   private boolean estaPrendido;
   private int canal;
   private int volumen;
-
-  public void prender() {
-    estaPrendido = true;
-  }
-
-  public void apagar() {
+  
+  public Televisor(){
     estaPrendido = false;
   }
-
-  public void subirVolumen() {
-    if (estaPrendido) {
-      volumen++;
-
-    } else {
-      System.out.println("No se puede subir el volumen del televisor");
-    }
-
+  public void prender(){
+    estaPrendido = true;
   }
-
-  public void bajarVolumen() {
-    if (estaPrendido) {
-      volumen--;
-
-    } else {
-      System.out.println("No se puede bajar el volumen del televisor");
-    }
+  
+  public void apagar(){
+    estaPrendido = false;
   }
-
-  public void cambiarCanal(int canal) {
-    if (estaPrendido) {
+  
+  public void subirVolumen(){
+    if(estaPrendido&&volumen<100){
+      volumen ++; //Esto es lo mismo que volumen = volumen + 1;
+    }else{
+      System.out.println("No se puede subir el volumen si el televisor no está prendido o si se superó el volúmen mínimo");
+    }
+    
+  }
+  
+  public void bajarVolumen(){
+    if(estaPrendido){
+      if(volumen>0){
+        volumen --; //Esto es lo mismo que volumen = volumen + 1;
+      }else{
+        System.out.println("No se puede seguir bajando el volúmen");
+      }
+    }else{
+      System.out.println("No se puede bajar el volumen si el televisor no está prendido");
+    }
+    
+  }
+  
+  public void cambiarCanal(int canal){
+    if(estaPrendido){
       this.canal = canal;
-    } else {
-      System.out.println("No se puede cambiar el canal del televisor");
+    }else{
+      System.out.println("No se puede cambiar el canal si el televisor no está prendido");
     }
   }
-
-  public int getVolumen() {
-    return volumen;
-  }
-
-  public int getcanal() {
+  
+  public int getCanal(){
     return canal;
   }
-
+  
+  public int getVolumen(){
+    return volumen;
+  }
+  
+  public boolean getEstado(){
+    return estaPrendido;
+  }
 }
